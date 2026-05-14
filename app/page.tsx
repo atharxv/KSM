331d25bc5d27d1432e07a039690c691f8f613402
jaywantadh <jaywantadhau16003@gmail.com>
2026-05-14 16:22:09 +0530

    Remove Best Seller section from the storefront

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "./components/HeroSection/HeroSection";
 import ActionBanner from "./components/ActionBanner/ActionBanner";
 import FeaturedCollection from "./components/FeaturedCollection/FeaturedCollection";
-import BestSeller from "./components/BestSeller/BestSeller";
 import DesignIdeology from "./components/DesignIdeology/DesignIdeology";
 import PhotoGallery from "./components/PhotoGallery/PhotoGallery";
 
@@ -11,7 +10,6 @@ export default function Home() {
       <HeroSection />
       <ActionBanner />
       <FeaturedCollection />
-      <BestSeller />
       <DesignIdeology />
       <PhotoGallery />
     </>
